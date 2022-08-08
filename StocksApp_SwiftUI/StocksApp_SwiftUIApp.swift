d98a399d0328746797998b5861b3e0aad4e95bce
Omotayo <omotayo4david@gmail.com>
2022-08-08 22:42:36 +0100

    Initial Commit

---
+++ StocksApp_SwiftUI/StocksApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp_SwiftUIApp.swift
+//  StocksApp_SwiftUI
+//
+//  Created by Omotayo on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
